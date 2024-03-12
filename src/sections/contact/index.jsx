e24b5affc84a0e3f9c6e69613754b5486397ce91
kacperwalter <52107523+kacperwalter@users.noreply.github.com>
2024-03-12 17:59:00 +0100

    Add some markup and styling for contact, add gap-horizontal spacing

--- src/sections/contact/index.jsx
+++ src/sections/contact/index.jsx
@@ -2,6 +2,7 @@ import './index.css';
 import Heading from '../../components/heading';
 import Text from '../../components/text';
 import Wrapper from '../../components/wrapper';
+import FormInput from '../../components/formInput';
 
 const Contact = () => {
 
@@ -13,14 +14,20 @@ const Contact = () => {
             Contact
           </Heading>
           <Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Nulla convallis libero nec nisl gravida, eget euismod quam 
-            aliquet.
+            Questions or concerns? Just fill out the form below and our support team will get back to you within 24 hours
           </Text>
         </div>
-        <div className="form">
-          some form
-        </div>
+
+        <form className="form">
+          <div className="divider">
+            <FormInput type="text" name="name" placeholder="First name" required />
+            <FormInput type="email" name="email" placeholder="Last name" required />
+          </div>
+
+          <FormInput type="text" name="subject" placeholder="Phone Number" required />
+          <FormInput type="textarea" name="message" placeholder="What Service are you interested in?" required />
+          <button type="submit">Send</button>
+        </form>
       </Wrapper>
     </section>
   )
