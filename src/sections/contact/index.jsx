cfd68ed58abf0f91211dbd72f8e511ec24b83fdb
kacperwalter <52107523+kacperwalter@users.noreply.github.com>
2024-03-12 17:08:05 +0100

    Refactor code styling in text component so it's the same as heading

--- src/sections/contact/index.jsx
+++ src/sections/contact/index.jsx
@@ -1,11 +1,28 @@
 import './index.css';
+import Heading from '../../components/heading';
+import Text from '../../components/text';
 
 const Contact = () => {
 
   return (
-    <div className="contact">
-      :)
-    </div>
+    <section className="contact">
+      <div className="container">
+        <div className="content">
+          <Heading level={2}>
+            Contact
+          </Heading>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Nulla convallis libero nec nisl gravida, eget euismod quam 
+            aliquet. Nullam nec erat auctor, condimentum elit eget, 
+            fermentum nunc. 
+          </Text>
+        </div>
+        <div className="form">
+          some form
+        </div>
+      </div>
+    </section>
   )
 };
 
