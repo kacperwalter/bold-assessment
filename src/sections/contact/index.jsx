b18240b695fc78149b9219b1d01323dda3deb7ad
kacperwalter <52107523+kacperwalter@users.noreply.github.com>
2024-03-12 23:05:36 +0100

    Cleaning and minor refactoring

--- src/sections/contact/index.jsx
+++ src/sections/contact/index.jsx
@@ -38,10 +38,10 @@ const Contact = () => {
         </div>
 
         <form className="form" onSubmit={handleSubmit}>
-          <div className="divider">
+          <span className="divider">
             <FormInput type="text" name="firstName" placeholder={contactModel.firstNamePlaceholder} required value={formData.firstName} onChange={handleChange} />
             <FormInput type="text" name="lastName" placeholder={contactModel.lastNamePlaceholder} required value={formData.lastName} onChange={handleChange} />
-          </div>
+          </span>
 
           <FormInput type="tel" name="phoneNumber" placeholder={contactModel.phoneNumberPlaceholder} required style="full" value={formData.phoneNumber} onChange={handleChange} />
           <FormInput type="textarea" name="message" placeholder={contactModel.messagePlaceholder} required style="full" value={formData.message} onChange={handleChange} />
